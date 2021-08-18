e25999fd730e9887fca78efa38d71e0c68097e2f
Božidar Bralić <bozidar.bralic@gmail.com>
2021-08-18 23:18:02 +0200

    CompleteProductUpsert

---
+++ Rocky/Controllers/ProductController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            //IEnumerable<Product> objList = _db.Product.Include(x => x.Category);
+            IEnumerable<Product> objList = _db.Product;
+
+            foreach (var obj in objList)
+            {
+                obj.Category = _db.Category.FirstOrDefault(x => x.Id == obj.CategoryId);
+            }
+
+            return View(objList);
+        }
+
+        // GET - UPSERT
+        public IActionResult Upsert(int? id)
+        {
+            IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            });
+
+            ViewBag.CategoryDropDown = CategoryDropDown;
+
+            Product product = new Product();
+            if (id == null)
+            {
+                // Then this is for create - možemo vratit prazni view ili s ovim novim product-om kojeg smo napravili
+                return View(product);
+            }
+            else
+            {
+                product = _db.Product.Find(id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product product)
+        {
+            return View();
+        }
+    }
+}
